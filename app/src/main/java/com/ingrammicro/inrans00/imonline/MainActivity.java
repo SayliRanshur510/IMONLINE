f00aad1925c805da08e9f3d4f66ad0a5c3d81ed9
Ranshur <Sayli.Ranshur@ingrammicro.com>
2018-09-07 15:11:29 +0530

    Initial commit

---
+++ app/src/main/java/com/ingrammicro/inrans00/imonline/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ingrammicro.inrans00.imonline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
