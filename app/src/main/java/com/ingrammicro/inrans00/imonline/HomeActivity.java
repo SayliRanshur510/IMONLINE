49a98daba27ba24cea00799e59095daf98c28187
Brady237 <Hemmady.Yash@ingrammicro.com>
2018-09-12 15:04:46 +0530

    Splash and Login first update

---
+++ app/src/main/java/com/ingrammicro/inrans00/imonline/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.ingrammicro.inrans00.imonline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ScrollView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        //ScrollView sv = (ScrollView)findViewById(R.id.scroll);
+        //sv.setEnabled(false);
+    }
+}
